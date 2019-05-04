b0cbedda66daf83f8b7c80de942dcdb5c30c7aba
Kevin O'Brien <kob123@hotmail.co.uk>
2019-05-04 13:34:35 +0100

    Rendered results using ejs

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
 app.get('/results', function(req, res){
-  request('http://www.omdbapi.com/?apikey=thewdb&s=california', function(error, response, body){
+  request('http://www.omdbapi.com/?apikey=thewdb&s=moon', function(error, response, body){
     if(!error && response.statusCode == 200) {
-      var results = JSON.parse(body);
-      res.send(results['Search'][0]['Title']);
+      var data = JSON.parse(body);
+      // The render method looks in the views folder for a file with the same name as the passed parameter.
+      // Pass an object with the data variable as a value of the data key.
+      res.render("results", {data: data});
     }
   })
 });
